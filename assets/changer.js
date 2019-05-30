d34ab0f76efb9ef9dfcbf7551c65b62ee9f05871
Iamnihc <34426008+Iamnihc@users.noreply.github.com>
2019-05-30 11:29:38 -0700

    added more info

--- assets/changer.js
+++ assets/changer.js
@@ -22,7 +22,7 @@ var info = [
   ["Planning for Attacks Begin", "In 1996, Khalid Sheikh Mohammed  joined al-Qaeda and started planning for another attack. He was able to convince the Taliban to aid him in his efforts. A long few years of planning with multiple people take place. KSM personally trains a group of operatives on how to infiltrate the US and blend in. As this training happens,  America is completely unaware of the attack."],
   ["World Trade Center Attacked", "In 2000, 19 men from Al-Qaeda entered the United States, 4 of whom were trained as pilots. September 11th,  2001 4 planes were hijacked. 2 were flown into the north and south Towers of the World Trade Center. Later a plane was rammed into the pentagon and another fell into a grassy field in Pennsylvania after passengers overtook the hijackers arn re-routed the collision."],
   ["Countermeasures Taken", "In 2002, New precautions are put into place, including a five-color threat system, card-access systems, metal detectors, and video cameras. The public was also encouraged to report anything that could be viewed as suspicious.  US starts working on countermeasures for cyber attacks in 2009. In May 27, 2010, the National Security Strategy guidelines were released by the DHS"],
-  ["Bin Laden Killed", "On May 2, 2011, Osama Bin Laden was killed by US special forces"]
+  ["Bin Laden Killed", "On May 2, 2011, Osama Bin Laden was killed by US special forces. This event was celebrated all across the United States."]
 ];
 
 var buttons = document.getElementById("bottom");
